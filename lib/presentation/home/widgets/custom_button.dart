cdd09aab07b73979628ac4cbc2ed6798a850c550
AmeeshaAntony <ameesha468@gmail.com>
2024-10-19 09:14:46 +0530

    sixth commit

--- lib/presentation/home/widgets/custom_button.dart
+++ lib/presentation/home/widgets/custom_button.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
    CustomButton({
-    super.key,required this.title,required this.icon
+    super.key,required this.title,required this.icon,
+    this.iconsize=30,
+    this.textsixe=18
   });
   final IconData icon;
   final String title;
+  final double iconsize;
+  final double textsixe;
 
   @override
   Widget build(BuildContext context) {
     return  Column(
       children: [
-        Icon(icon,color: Colors.white,size: 25,),
-        Text(title,style: TextStyle(fontSize: 20,),)
+        Icon(icon,color: Colors.white,size: iconsize,),
+        Text(title,style: TextStyle(fontSize: textsixe,),)
       ],
     );
   }
--- lib/presentation/new_hot/screen_new_hot.dart
+++ lib/presentation/new_hot/screen_new_hot.dart
@@ -65,78 +65,123 @@ class ScreenNewHot extends StatelessWidget {
   }
 
   Widget _buildcomingsoon(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return ListView(children: [
-      kheight,
-      Row(
-        children: [
-          const SizedBox(
-            width: 50,
-            height: 500,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text('FEB',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey)),
-                Text(
-                  '11',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            width: size.width - 50,
-            height: 500,
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 250,
-                      child: Image.network(
-                        "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ykocDqwg5PRUopnBVb1x1DWnEJF.jpg",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,right: 10,
-                      child: CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.black12,
-                          child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.volume_off,
-                                color: Colors.white,
-                                size: 20,
-                              ))),
-                    ),
-                  ],
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('TallGirl2',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
-                    Row(
-                      children: [
-                        CustomButton(title: 'Remind me', icon: Icons.alarm)
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    ]);
+    
+    return List
   }
 
   Widget _buildeveryone() {
     return SizedBox();
+  }
+}
+
+class ComingSoon extends StatelessWidget {
+  const ComingSoon({
+    super.key,
+    
+  });
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(
+          width: 50,
+          height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('FEB',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey)),
+              Text(
+                '11',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 100,
+          height: 500,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 250,
+                    child: Image.network(
+                      "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ykocDqwg5PRUopnBVb1x1DWnEJF.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 10,
+                    child: CircleAvatar(
+                        radius: 22,
+                        backgroundColor: Colors.black12,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.volume_off,
+                              color: Colors.white,
+                              size: 20,
+                            ))),
+                  ),
+                ],
+              ),
+              //const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'TallGirl2',
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      CustomButton(
+                        title: 'Remind me',
+                        icon: Icons.all_out_sharp,
+                        iconsize: 20,
+                        textsixe: 15,
+                      ),
+                      kwidth,
+                      CustomButton(
+                        title: 'Info',
+                        icon: Icons.info,
+                        iconsize: 20,
+                        textsixe: 15,
+                      ),
+                      kwidth
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("Coming on Friday"),
+              kheight,
+              const Text("Tall Girl 2",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )),
+              kheight,
+              Text(
+                  'Landing the lead in the school musical is a dream come true for jodi,until the pressure sends her confidence - and her relationship - into a tallspain',
+                  style: TextStyle(color: Colors.grey))
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
